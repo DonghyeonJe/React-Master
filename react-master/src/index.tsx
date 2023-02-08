f4eda8d817318bc45c543d5b80453febffc21d5c
donghyeon <wlalqjrj@gmail.com>
2023-02-08 22:07:21 +0900

    0208 night

--- react-master/src/index.tsx
+++ react-master/src/index.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { ThemeProvider } from "styled-components";
 import App from "./App";
+import { theme } from "./theme";
 
 const container = document.getElementById("root");
 const root = createRoot(container!); // createRoot(container!) if you use TypeScript
 
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
